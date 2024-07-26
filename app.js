de24f90e0ce9e649ed3009cc24db14b0a291243b
Sarvesh Rawal <sarveshrawal.2006@gmail.com>
2024-07-26 16:15:35 +0530

    added dynamic weather image and background

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-let latt, long;
+let latt, long, weather;
 let riseDiv = document.querySelector('.srise');
 let setDiv = document.querySelector('.sset');
 let humidDiv =  document.querySelector('.humid');
 let windDiv =  document.querySelector('.wind-speed');
 let tempDiv = document.querySelector('.temp');
+let loc_heading = document.querySelector('.location-heading');
+let weatherPara = document.querySelector('.weather-info');
+let weather_img = document.querySelector('.weather-symbol');
+let bgvid = document.querySelector('.bgvid');
 
 const geoCode = async() => {
     let loc = document.querySelector('.loc input');
     let state = document.querySelector('.state input');
-    console.log(loc.value, state.value);
+    loc_heading.innerText = (loc.value) + ', ' + state.value;
     let geo_key = '66a0c1671a13d634910942eakac44e3';
     let geo_api = `https://geocode.maps.co/search?q=${loc.value} ${state.value}&api_key=${geo_key}`;
 
     let response = await fetch(geo_api);
     let data = await response.json();
-    console.log(data);
     latt = data[0].lat;
     long = data[0].lon;
-    await weather();
+    await weather_app();
+    await weather_icon_change();
 }
 
-const weather = async() => {
+const weather_app = async() => {
     let weather_key = '588667189f67f789b65877c440714add';
     let weather_api = 'https://api.openweathermap.org/data/2.5/weather?';
     const url = `${weather_api}lat=${latt}&lon=${long}&appid=${weather_key}&units=metric`;
@@ -34,10 +38,8 @@ const weather = async() => {
     humidDiv.innerText = humidity + '%';
     
     let feels_like = data.main.feels_like;
-    console.log('feels like',feels_like);
-    
-    let weather = data.weather[0].main;
-    console.log('weather',weather);
+    weather = data.weather[0].main;
+    weatherPara.innerText = weather + ', Feels like ' + feels_like;
 
     let sunRise = data.sys.sunrise;
     let ist_sunrise = time_converter(sunRise);
@@ -57,6 +59,32 @@ const time_converter = (initTime) => {
     let hrs = date.getHours().toString();
     let min = date.getMinutes().toString();
     return hrs + ':' +  min;
+}
+
+const weather_icon_change = () => {
+    switch (weather) {
+        case 'Sun':
+            weather_img.src = 'sun-removebg-preview.png';
+            bgvid.src = 'sunny.mp4';
+            break;
+        case 'Rain':
+            weather_img.src = 'rain-removebg-preview.png';    
+            bgvid.src = 'rain.mp4';
+            break;
+        case 'Clouds':
+            weather_img.src = 'cloudy-removebg-preview.png';
+            bgvid.src = 'cloudy.mp4';
+            break;
+        case 'Mist':
+        case 'Fog': 
+            weather_img.src = 'fog-removebg-preview.png';    
+            bgvid.src = 'mist.mp4';
+            break;
+        default:
+            weather_img.src = 'fog-removebg-preview.png';    
+            bgvid.src = 'mist.mp4';
+            break;
+    }
 }
 
 let btn = document.querySelector('.sub');
